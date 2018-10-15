fe253eb52586d55183149d746006aea4d73ce478
Tom Minka <8955276+tminka@users.noreply.github.com>
2018-10-15 11:42:01 +0100

    TwoCoins example uses 'engine'.

--- test/TestFSharp/TwoCoins.fs
+++ test/TestFSharp/TwoCoins.fs
@@ -26,13 +26,13 @@ module coins =
         let bothHeads = firstCoin &&& secondCoin
 
         // The inference
-        let ie = InferenceEngine()
+        let engine = InferenceEngine()
 
-        let bothHeadsPost = ie.Infer<Bernoulli>(bothHeads)
+        let bothHeadsPost = engine.Infer<Bernoulli>(bothHeads)
         printfn "Both heads posterior = %A" bothHeadsPost
         bothHeads.ObservedValue <- false
 
-        let firstCoinPost = ie.Infer<Bernoulli>(firstCoin)
+        let firstCoinPost = engine.Infer<Bernoulli>(firstCoin)
         printfn "First coin posterior = %A" firstCoinPost
 
 
