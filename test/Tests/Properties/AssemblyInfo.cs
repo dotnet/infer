1c6e97e6fb35dd00ffb008ea83fe373d9f8ae719
Tom Minka <8955276+tminka@users.noreply.github.com>
2018-12-03 19:46:33 +0000

    BlogTests.Handedness is a test

--- test/Tests/Properties/AssemblyInfo.cs
+++ test/Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Infer2Tests")]
+[assembly: AssemblyTitle("Microsoft.ML.Probabilistic.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Research Limited")]
