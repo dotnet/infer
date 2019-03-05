7ac781abd531c516a78f5ea53be2f8755e494257
Tom Minka <8955276+tminka@users.noreply.github.com>
2019-03-05 15:45:46 +0000

    Added ReviewerCalibration example

---
+++ src/Examples/ReviewerCalibration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReviewerCalibration
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add your data here
+            Review[] reviews = new Review[0];
+            ReviewerCalibration reviewerCalibration = new ReviewerCalibration();
+            var results = reviewerCalibration.Run(reviews);
+            // Display the results here
+        }
+    }
+}
