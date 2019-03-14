c2aae70dfae0ccee82aee0d4e02f13e053bda458
John Guiver <joguiver@microsoft.com>
2019-03-14 13:14:56 +0000

    TrySetToConstantOnSupportOfLog implementation

--- src/Runtime/Factors/StringFormatOp.cs
+++ src/Runtime/Factors/StringFormatOp.cs
@@ -503,14 +503,7 @@ namespace Microsoft.ML.Probabilistic.Factors
                 StringAutomaton validatingAutomaton = GetArgumentValidatingAutomaton(i, argNames);
                 result.SetToProduct(i == 0 ? format : result, validatingAutomaton);
                 result.ClearGroups();
-                try
-                {
-                    result.SetToConstantOnSupportOfLog(0.0, result);
-                }
-                catch (NotImplementedException)
-                {
-                    // ignored
-                }
+                result.SetToConstantOnSupportOfLog(0.0, result);
             }
 
             return result;
