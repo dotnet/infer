e4110925851964d9b9630afef391aa31236c0b60
Vijay Sharma <v-vijsha@microsoft.com>
2019-06-22 03:43:13 +0100

    - Updated Loki_BernoulliEnterTest to include logging to test output - Updated Loki

--- test/Infer.Loki.Tests/LokiGateOpTests.cs
+++ test/Infer.Loki.Tests/LokiGateOpTests.cs
@@ -2,23 +2,34 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Loki.Extensions;
+using System;
+using System.Diagnostics;
 using System.IO;
-using Loki;
-using Microsoft.ML.Probabilistic.Tests;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Loki;
+using Microsoft.ML.Probabilistic.Tests;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Infer.Loki.Tests
 {
     /// <summary>
-    /// Tests for gate operations.
+    ///     Tests for gate operations.
     /// </summary>
-    public class LokiGateOpTests
+    public class LokiGateOpTests : IDisposable
     {
+        private readonly XunitTraceListener listener;
+        private bool disposed;
+
+        public LokiGateOpTests(ITestOutputHelper output)
+        {
+            this.listener = new XunitTraceListener(output);
+            Trace.Listeners.Add(this.listener);
+        }
+
         /// <summary>
-        /// Tests EP and BP gate exit ops for Bernoulli random variable for correctness.
+        ///     Tests EP and BP gate exit ops for Bernoulli random variable for correctness.
         /// </summary>
         [Fact]
         public async Task Loki_BernoulliEnterTest()
@@ -59,11 +70,54 @@ namespace Infer.Loki.Tests
             settings.ExcludeProject(new Regex(@"^Tools\.PrepareSource"));
             settings.ExcludeProject(new Regex("^Tutorials"));
             settings.ExcludeProject(new Regex("^Visualizers"));
-            
+
             var testResult = await TestRunner.BuildAndRunTest(settings, new GateOpTests().BernoulliEnterTest);
 
             Assert.True(testResult.DoublePrecisionPassed);
             Assert.True(testResult.HighPrecisionPassed);
+        }
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Trace.Listeners.Remove(this.listener);
+            }
+
+            this.disposed = true;
+        }
+    }
+
+    public class XunitTraceListener : TraceListener
+    {
+        private readonly ITestOutputHelper output;
+
+        public XunitTraceListener(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        public override void Write(string str)
+        {
+            this.output.WriteLine(str);
+        }
+
+        public override void WriteLine(string str)
+        {
+            this.output.WriteLine(str);
         }
     }
 }
