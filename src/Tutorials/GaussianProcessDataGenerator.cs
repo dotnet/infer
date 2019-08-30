480865a8ab26ef2ce1a5e81c07580a2229905bb7
Philip Ball <t-phball@microsoft.com>
2019-08-30 16:48:55 +0100

    refactor generator into a separate function; redo plotting; simpler run script with preprocessor directives

--- src/Tutorials/GaussianProcessDataGenerator.cs
+++ src/Tutorials/GaussianProcessDataGenerator.cs
@@ -6,10 +6,12 @@ using Microsoft.ML.Probabilistic.Models;
 using Microsoft.ML.Probabilistic.Math;
 using Microsoft.ML.Probabilistic.Distributions;
 using Microsoft.ML.Probabilistic.Distributions.Kernels;
+using System;
+#if oxyplot
 using OxyPlot;
 using OxyPlot.Wpf;
-using System;
 using System.Threading;
+#endif
 
 namespace Microsoft.ML.Probabilistic.Tutorials
 {
@@ -19,26 +21,30 @@ namespace Microsoft.ML.Probabilistic.Tutorials
     /// 2) pick a random subset of 'numData' points;
     /// 3) pick a further random proportion 'propCorrupt' of 'numData' to corrupt according to a uniform distribution with a range of -3 to 3
     /// </summary>
-    [Example("Applications", "A Gaussian Process regression example")]
+    [Example("Applications", "A Gaussian Process data generator with random point corruption")]
     class GaussianProcessDataGenerator
     {
+        // Path for the results plot
+        private const string OutputPlotPath = @"..\..\..\TutorialData\GPRandomDataGenerator.png";
+
         public void Run()
+        {
+            (Vector[] dataX, double[] dataY) = GenerateRandomData(30, 0.3);
+#if oxyplot
+            PlotScatter(dataX, dataY);
+#endif
+        }
+
+        public (Vector[] dataX, double[] dataY) GenerateRandomData(int numData, double proportionCorrupt)
         {
             InferenceEngine engine = new InferenceEngine();
             if (!(engine.Algorithm is Algorithms.ExpectationPropagation))
             {
-                Console.WriteLine("This example only runs with Expectation Propagation");
-                return;
+                throw new ArgumentException("This example only runs with Expectation Propagation");
             }
 
-            // Number of datapoints
-            int numData = 30;
-
-            // The proportion of points to randomly corrupt
-            double propCorrupt = 0.3;
-
             // The points to evaluate
-            Vector[] inputs = this.VectorRange(-5, 5, numData, true);
+            Vector[] randomInputs = this.VectorRange(-5, 5, numData, true);
 
             // Set up the GP prior, a distribution over functions, which will be filled in later
             Variable<SparseGP> prior = Variable.New<SparseGP>().Named("prior");
@@ -47,7 +53,7 @@ namespace Microsoft.ML.Probabilistic.Tutorials
             Variable<IFunction> f = Variable<IFunction>.Random(prior).Named("f");
 
             // The locations to evaluate the function
-            VariableArray<Vector> x = Variable.Observed(inputs).Named("x");
+            VariableArray<Vector> x = Variable.Observed(randomInputs).Named("x");
             Range j = x.Range.Named("j");
 
             // The observation model
@@ -68,46 +74,30 @@ namespace Microsoft.ML.Probabilistic.Tutorials
             SparseGP sgp = engine.Infer<SparseGP>(f);
             var randomFunc = sgp.Sample();
 
-            // plotting boilerplate
-            var p1 = new OxyPlot.Series.ScatterSeries
-            {
-                Title = "Random Function"
-            };
-
             Random rng = new Random();
 
+            double[] randomOutputs = new double[randomInputs.Length];
+
             // get random data
-            for (int i = 0; i < inputs.Length; i++)
+            for (int i = 0; i < randomInputs.Length; i++)
             {
-                double post = randomFunc.Evaluate(inputs[i]);
+                double post = randomFunc.Evaluate(randomInputs[i]);
                 // corrupt data point if it we haven't exceed the proportion we wish to corrupt
-                if (i < propCorrupt * numData)
+                if (i < proportionCorrupt * numData)
                 {
                     double sign = rng.NextDouble() > 0.5 ? 1 : -1;
                     double distance = rng.NextDouble() * 3;
                     post = (sign * distance) + post;
                 }
 
-                p1.Points.Add(new OxyPlot.Series.ScatterPoint(inputs[i][0], post));
+                randomOutputs[i] = post;
             }
 
-            var model = new PlotModel();
-            model.Series.Add(p1);
+            int numCorrupted = (int)System.Math.Ceiling(numData * proportionCorrupt);
+            Console.WriteLine("Model complete: Generated {0} points with {1} corrupted", numData, numCorrupted);
 
-            Thread thread = new Thread(() => DisplayPNG(model));
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-
-            int numCorrupted = (int)System.Math.Ceiling(numData * propCorrupt);
-            Console.WriteLine("Plotting complete: Generated {0} points with {1} corrupted", numData, numCorrupted);
-        }
-
-        private void DisplayPNG(PlotModel model)
-        {
-            var a = Thread.CurrentThread.GetApartmentState();
-            var outputToFile = "test-oxyplot-file.png";
-            PngExporter.Export(model, outputToFile, 600, 400, OxyColors.White);
-        }
+            return (randomInputs, randomOutputs);
+        } 
 
         /// <summary>
         /// Generates a 1D vector with length len having a min and max; data points are randomly distributed and ordered if specified
@@ -136,5 +126,38 @@ namespace Microsoft.ML.Probabilistic.Tutorials
 
             return inputs;
         }
+
+#if oxyplot
+        private void PlotScatter(Vector[] dataX, double[] dataY)
+        {
+            var scatterSeries = new OxyPlot.Series.ScatterSeries { Title = "Random data" };
+
+            for (int i = 0; i < dataX.Length; i++)
+            {
+                scatterSeries.Points.Add(new OxyPlot.Series.ScatterPoint(dataX[i][0], dataY[i]));
+            }
+
+            var model = new PlotModel();
+            model.Title = "Random Function drawn from a GP with corrupted data points";
+            model.Series.Add(scatterSeries);
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                Title = "x"
+            });
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Left,
+                Title = "y"
+            });
+
+            // Required for plotting
+            Thread thread = new Thread(() => PngExporter.Export(model, OutputPlotPath, 600, 400, OxyColors.White));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+            Console.WriteLine("Saved PNG to {0}", OutputPlotPath);
+        }
+#endif
     }
 }
