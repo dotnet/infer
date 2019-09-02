d856e81da78de1ac46fa6a67a3d3f712f97e93e3
Philip Ball <t-phball@microsoft.com>
2019-09-02 19:51:11 +0100

    finish end-to-end code

---
+++ src/Examples/RobustGaussianProcess/GaussianProcessRegressor.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.ML.Probabilistic.Distributions;
+using Microsoft.ML.Probabilistic.Math;
+using Microsoft.ML.Probabilistic.Models;
+using System;
+
+namespace RobustGaussianProcess
+{
+    public class GaussianProcessRegressor
+    {
+        public Variable<SparseGP> Prior { get; }
+        public Variable<bool> Evidence { get; }
+        public Variable<IFunction> F { get; }
+        public IfBlock Block;
+        public VariableArray<Vector> X { get; }
+        public Range J;
+        public Variable<double> Score { get; }
+
+        public GaussianProcessRegressor(Vector[] trainingInputs)
+        {
+            // Modelling code
+            Evidence = Variable.Bernoulli(0.5).Named("evidence");
+            Block = Variable.If(Evidence);
+            Prior = Variable.New<SparseGP>().Named("prior");
+            F = Variable<IFunction>.Random(Prior).Named("f");
+            X = Variable.Observed(trainingInputs).Named("x");
+            J = X.Range.Named("j");
+        }
+
+        public GaussianProcessRegressor(Vector[] trainingInputs, bool useStudentTLikelihood, double[] trainingOutputs) : this(trainingInputs)
+        {
+            VariableArray<double> y = Variable.Observed(trainingOutputs, J).Named("y");
+
+            if (!useStudentTLikelihood)
+            {
+                // Standard Gaussian Process
+                Console.WriteLine("Training a Gaussian Process regressor");
+                var score = GetScore(X, F, J);
+                y[J] = Variable.GaussianFromMeanAndVariance(score, 0.8);
+            }
+            else
+            {
+                // Gaussian Process with Student-t likelihood
+                Console.WriteLine("Training a Gaussian Process regressor with Student-t likelihood");
+                var noisyScore = GetNoisyScore(X, F, J, trainingOutputs);
+                y[J] = Variable.GaussianFromMeanAndVariance(noisyScore[J], 0.8);
+            }
+        }
+
+        private static Variable<double> GetScore(VariableArray<Vector> x, Variable<IFunction> f, Range j)
+        {
+            return Variable.FunctionEvaluate(f, x[j]);
+        }
+
+        private static VariableArray<double> GetNoisyScore(VariableArray<Vector> x, Variable<IFunction> f, Range j, double[] trainingOutputs)
+        {
+            // The student-t distribution arises as the mean of a normal distribution once an unknown precision is marginalised out
+            Variable<double> score = GetScore(x, f, j);
+            VariableArray<double> noisyScore = Variable.Observed(trainingOutputs, j).Named("noisyScore");
+            using (Variable.ForEach(j))
+            {
+                // The precision of the Gaussian is modelled with a Gamma distribution
+                var precision = Variable.GammaFromShapeAndRate(4, 1).Named("precision");
+                noisyScore[j] = Variable.GaussianFromMeanAndPrecision(score, precision);
+            }
+            return noisyScore;
+        }
+    }
+}
--- src/Examples/RobustGaussianProcess/Program.cs
+++ src/Examples/RobustGaussianProcess/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿#define oxyplot
+using Microsoft.ML.Probabilistic.Distributions;
+using Microsoft.ML.Probabilistic.Distributions.Kernels;
+using Microsoft.ML.Probabilistic.Math;
+using Microsoft.ML.Probabilistic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +13,54 @@ namespace RobustGaussianProcess
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            FitDataset(true);
+            FitDataset(false);
+        }
+
+        static void FitDataset(bool fitAIS)
+        {
+            Vector[] trainingInputs;
+            double[] trainingOutputs;
+            string datasetName;
+
+            if (fitAIS)
+            {
+                datasetName = "AIS";
+                var trainingData = Utilities.LoadAISDataset();
+                trainingInputs = trainingData.Select(tup => Vector.FromArray(new double[1] { tup.x })).ToArray();
+                trainingOutputs = trainingData.Select(tup => tup.y).ToArray();
+            }
+            else
+            {
+                datasetName = "Generated";
+                (trainingInputs, trainingOutputs) = GaussianProcessDataGenerator.GenerateRandomData(30, 0.3);
+            }
+
+            InferenceEngine engine = Utilities.GetInferenceEngine();
+
+            for (var i = 0; i < 2; i++)
+            {
+                var gaussianProcessRegressor = new GaussianProcessRegressor(trainingInputs, i != 0, trainingOutputs);
+                gaussianProcessRegressor.Block.CloseBlock();
+
+                // Log length scale estimated as -1
+                var kf = new SquaredExponential(-1);
+                GaussianProcess gp = new GaussianProcess(new ConstantFunction(0), kf);
+
+                // Convert SparseGP to full Gaussian Process by evaluating at all the training points
+                gaussianProcessRegressor.Prior.ObservedValue = new SparseGP(new SparseGPFixed(gp, trainingInputs.ToArray()));
+                double logOdds = engine.Infer<Bernoulli>(gaussianProcessRegressor.Evidence).LogOdds;
+                Console.WriteLine("{0} evidence = {1}", kf, logOdds.ToString("g4"));
+
+                // Infer the posterior Sparse GP
+                SparseGP sgp = engine.Infer<SparseGP>(gaussianProcessRegressor.F);
+
+#if oxyplot
+                Utilities.PlotPredictions(sgp, trainingInputs, trainingOutputs, i != 0, datasetName);
+#endif
+            }
         }
     }
 }
