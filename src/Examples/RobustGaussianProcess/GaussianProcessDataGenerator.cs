0d462b001668a7a576d053c594df41415c6283af
Philip Ball <t-phball@microsoft.com>
2019-09-03 10:37:45 +0100

    set random seed for random data

--- src/Examples/RobustGaussianProcess/GaussianProcessDataGenerator.cs
+++ src/Examples/RobustGaussianProcess/GaussianProcessDataGenerator.cs
@@ -15,16 +15,21 @@ namespace RobustGaussianProcess
     {
         public static (Vector[] dataX, double[] dataY) GenerateRandomData(int numData, double proportionCorrupt)
         {
+            int randomSeed = 9876;
+
+            Random rng = new Random(randomSeed);
+            Rand.Restart(randomSeed);
+
             InferenceEngine engine = Utilities.GetInferenceEngine();
 
             // The points to evaluate
-            Vector[] randomInputs = Utilities.VectorRange(0, 1, numData, true);
+            Vector[] randomInputs = Utilities.VectorRange(0, 1, numData, null);
 
             var gaussianProcessGenerator = new GaussianProcessRegressor(randomInputs);
             gaussianProcessGenerator.Block.CloseBlock();
 
             // The basis
-            Vector[] basis = Utilities.VectorRange(0, 1, 6, false);
+            Vector[] basis = Utilities.VectorRange(0, 1, 6, rng);
 
             // The kernel
             var kf = new SquaredExponential(-1);
@@ -37,7 +42,6 @@ namespace RobustGaussianProcess
             SparseGP sgp = engine.Infer<SparseGP>(gaussianProcessGenerator.F);
             var randomFunc = sgp.Sample();
 
-            Random rng = new Random();
 
             double[] randomOutputs = new double[randomInputs.Length];
             int numCorrupted = (int)System.Math.Ceiling(numData * proportionCorrupt);
