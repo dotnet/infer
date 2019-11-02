f008481029915903c8a9dc5701eee8573af87567
Ivan Korostelev <ivkorost@microsoft.com>
2019-11-02 09:18:16 +0000

    Fix DiscreteChar.Complement()

--- src/Runtime/Distributions/DiscreteChar.cs
+++ src/Runtime/Distributions/DiscreteChar.cs
@@ -1537,8 +1537,9 @@ namespace Microsoft.ML.Probabilistic.Distributions
                     if (range.StartInclusive != prevEnd)
                     {
                         builder.AddRange(new CharRange(prevEnd, range.StartInclusive, Weight.One));
-                        prevEnd = range.EndExclusive;
                     }
+
+                    prevEnd = range.EndExclusive;
                 }
 
                 if (prevEnd != CharRangeEndExclusive)
