84970a3cb479ad2fb3860f2cc514edc67ca9ebf6
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2020-01-31 12:29:10 +0300

    Fixed serires generation tool

--- src/Tools/GenerateSeries/GenerateSeries.py
+++ src/Tools/GenerateSeries/GenerateSeries.py
@@ -2,59 +2,77 @@ from __future__ import division
 from sympy import zeta, evalf, bernoulli, symbols, Poly, series, factorial, factorial2, S, log, exp
 
 # configuration
-decimal_precision = 20
+decimal_precision = 24
 
 gamma_at_2_series_length = 26
 gamma_at_2_variable_name = "dx"
+gamma_at_2_indent = "                    "
 
 digamma_at_2_series_length = 26
 digamma_at_2_variable_name = "dx"
+digamma_at_2_indent = "                    "
 
 digamma_asymptotic_series_length = 8
-digamma_asymptotic_variable_name = "dx"
+digamma_asymptotic_variable_name = "invX2"
+digamma_asymptotic_indent = "                "
 
 trigamma_at_1_series_length = 2
-trigamma_at_1_variable_name = "dx"
+trigamma_at_1_variable_name = "x"
+trigamma_at_1_indent = "                    "
 
 trigamma_asymptotic_series_length = 9
-trigamma_asymptotic_variable_name = "dx"
+trigamma_asymptotic_variable_name = "invX2"
+trigamma_asymptotic_indent = "                    "
 
 tetragamma_at_1_series_length = 2
-tetragamma_at_1_variable_name = "dx"
+tetragamma_at_1_variable_name = "x"
+tetragamma_at_1_indent = "                    "
 
 tetragamma_asymptotic_series_length = 9
-tetragamma_asymptotic_variable_name = "dx"
+tetragamma_asymptotic_variable_name = "invX2"
+tetragamma_asymptotic_indent = "                "
 
 gammaln_asymptotic_series_length = 7
-gammaln_asymptotic_variable_name = "dx"
+gammaln_asymptotic_variable_name = "invX2"
+gammaln_asymptotic_indent = "                    "
 
 log_1_plus_series_length = 6
-log_1_plus_variable_name = "dx"
+log_1_plus_variable_name = "x"
+log_1_plus_indent = "                    "
 
 log_1_minus_series_length = 5
-log_1_minus_variable_name = "dx"
+log_1_minus_variable_name = "y"
+log_1_minus_indent = "                    "
 
 x_minus_log_1_plus_series_length = 7
-x_minus_log_1_plus_variable_name = "dx"
+x_minus_log_1_plus_variable_name = "xOverAMinus1"
+x_minus_log_1_plus_indent = "                    "
 
 exp_minus_1_series_length = 5
-exp_minus_1_variable_name = "dx"
+exp_minus_1_variable_name = "x"
+exp_minus_1_indent = "                    "
 
 exp_minus_1_ratio_minus_1_ratio_minus_half_series_length = 13
-exp_minus_1_ratio_minus_1_ratio_minus_half_variable_name = "dx"
+exp_minus_1_ratio_minus_1_ratio_minus_half_variable_name = "x"
+exp_minus_1_ratio_minus_1_ratio_minus_half_indent = "                    "
 
 log_exp_minus_1_ratio_series_length = 5
-log_exp_minus_1_ratio_variable_name = "dx"
+log_exp_minus_1_ratio_variable_name = "x"
+log_exp_minus_1_ratio_indent = "                    "
 
 normcdfln_asymptotic_series_length = 8
-normcdfln_asymptotic_variable_name = "dx"
+normcdfln_asymptotic_variable_name = "z"
+normcdfln_asymptotic_indent = "                    "
+
+def formatRealCoefficient(coefficient):
+    return f"%0.{decimal_precision}g" % coefficient
 
 def printPolyFromNumbers(varname, coefficients, indent):
     if len(coefficients) <= 1:
-        print(coefficients[0])
+        print(f"{indent}{formatRealCoefficient(coefficients[0])}")
         return
     if coefficients[0] != 0.0:
-        print(f"{indent}{coefficients[0]} +")
+        print(f"{indent}{formatRealCoefficient(coefficients[0])} +")
     last_non_zero_idx = len(coefficients) - 1
     while coefficients[last_non_zero_idx] == 0.0:
         last_non_zero_idx = last_non_zero_idx - 1
@@ -64,11 +82,11 @@ def printPolyFromNumbers(varname, coefficients, indent):
     while idx < last_non_zero_idx:
         print(f"{varname} * ", end='')
         if coefficients[idx] != 0.0:
-            print(f"({coefficients[idx]} +")
+            print(f"({formatRealCoefficient(coefficients[idx])} +")
             print(indent, end='')
             parentheses = parentheses + 1
         idx = idx + 1
-    print(f"{varname} * {coefficients[last_non_zero_idx]}", end='')
+    print(f"{varname} * {formatRealCoefficient(coefficients[last_non_zero_idx])}", end='')
     for i in range(0, parentheses):
         print(")", end='')
     print()
@@ -78,7 +96,7 @@ def formatRationalCoefficient(coefficient):
 
 def printPolyFromRationals(varname, coefficients, indent):
     if len(coefficients) <= 1:
-        print(formatRationalCoefficient(coefficients[0]))
+        print(f"{indent}{formatRationalCoefficient(coefficients[0])}")
         return
     if coefficients[0] != 0:
         print(f"{indent}{formatRationalCoefficient(coefficients[0])} +")
@@ -108,7 +126,7 @@ def gamma_at_2_coefficient(k):
 def digamma_at_2_coefficient(k):
     if k == 0:
         return 0.0
-    return ((-1)**(k + 1)*(zeta(k + 1) - 1)/(k + 1)/(k + 1)).evalf(decimal_precision)
+    return ((-1)**(k + 1)*(zeta(k + 1) - 1)).evalf(decimal_precision)
 
 def digamma_asymptotic_coefficient(k):
     if k == 0:
@@ -204,62 +222,62 @@ def normcdfln_asymptotic_coefficient(m):
 def main():
     print("(1) - Gamma at 2:")
     gamma_at_2_coefficients = [gamma_at_2_coefficient(k) for k in range(0, gamma_at_2_series_length)]
-    printPolyFromNumbers(gamma_at_2_variable_name, gamma_at_2_coefficients, "                    ")
+    printPolyFromNumbers(gamma_at_2_variable_name, gamma_at_2_coefficients, gamma_at_2_indent)
 
     print("(2) - DiGamma at 2:")
     digamma_at_2_coefficients = [digamma_at_2_coefficient(k) for k in range(0, digamma_at_2_series_length)]
-    printPolyFromNumbers(digamma_at_2_variable_name, digamma_at_2_coefficients, "                    ")
+    printPolyFromNumbers(digamma_at_2_variable_name, digamma_at_2_coefficients, digamma_at_2_indent)
 
     print("(3) - DiGamma asymptotic:")
     digamma_asymptotic_coefficients = [digamma_asymptotic_coefficient(k) for k in range(0, digamma_asymptotic_series_length)]
-    printPolyFromRationals(digamma_asymptotic_variable_name, digamma_asymptotic_coefficients, "                    ")
+    printPolyFromRationals(digamma_asymptotic_variable_name, digamma_asymptotic_coefficients, digamma_asymptotic_indent)
 
     print("(4) - TriGamma at 1:")
     trigamma_at_1_coefficients = [trigamma_at_1_coefficient(k) for k in range(0, trigamma_at_1_series_length)]
-    printPolyFromNumbers(trigamma_at_1_variable_name, trigamma_at_1_coefficients, "                    ")
+    printPolyFromNumbers(trigamma_at_1_variable_name, trigamma_at_1_coefficients, trigamma_at_1_indent)
 
     print("(5) - TriGamma asymptotic:")
     trigamma_asymptotic_coefficients = [trigamma_asymptotic_coefficient(k) for k in range(0, trigamma_asymptotic_series_length)]
-    printPolyFromRationals(trigamma_asymptotic_variable_name, trigamma_asymptotic_coefficients, "                    ")
+    printPolyFromRationals(trigamma_asymptotic_variable_name, trigamma_asymptotic_coefficients, trigamma_asymptotic_indent)
 
     print("(6) - TetraGamma at 1:")
     tetragamma_at_1_coefficients = [tetragamma_at_1_coefficient(k) for k in range(0, tetragamma_at_1_series_length)]
-    printPolyFromNumbers(tetragamma_at_1_variable_name, tetragamma_at_1_coefficients, "                    ")
+    printPolyFromNumbers(tetragamma_at_1_variable_name, tetragamma_at_1_coefficients, tetragamma_at_1_indent)
 
     print("(7) - TetraGamma asymptotic:")
     tetragamma_asymptotic_coefficients = [tetragamma_asymptotic_coefficient(k) for k in range(0, tetragamma_asymptotic_series_length)]
-    printPolyFromRationals(tetragamma_asymptotic_variable_name, tetragamma_asymptotic_coefficients, "                    ")
+    printPolyFromRationals(tetragamma_asymptotic_variable_name, tetragamma_asymptotic_coefficients, tetragamma_asymptotic_indent)
 
     print("(8) - GammaLn asymptotic:")
     gammaln_asymptotic_coefficients = [gammaln_asymptotic_coefficient(k) for k in range(0, gammaln_asymptotic_series_length)]
-    printPolyFromRationals(gammaln_asymptotic_variable_name, gammaln_asymptotic_coefficients, "                    ")
+    printPolyFromRationals(gammaln_asymptotic_variable_name, gammaln_asymptotic_coefficients, gammaln_asymptotic_indent)
 
     print("(9) - log(1 + x):")
     log_1_plus_coefficients = [log_1_plus_coefficient(k) for k in range(0, log_1_plus_series_length)]
-    printPolyFromRationals(log_1_plus_variable_name, log_1_plus_coefficients, "                    ")
+    printPolyFromRationals(log_1_plus_variable_name, log_1_plus_coefficients, log_1_plus_indent)
 
     print("(10) - log(1 - x):")
     log_1_minus_coefficients = [log_1_minus_coefficient(k) for k in range(0, log_1_minus_series_length)]
-    printPolyFromRationals(log_1_minus_variable_name, log_1_minus_coefficients, "                    ")
+    printPolyFromRationals(log_1_minus_variable_name, log_1_minus_coefficients, log_1_minus_indent)
 
     print("(11) - x - log(1 + x):")
     x_minus_log_1_plus_coefficients = [x_minus_log_1_plus_coefficient(k) for k in range(0, x_minus_log_1_plus_series_length)]
-    printPolyFromRationals(x_minus_log_1_plus_variable_name, x_minus_log_1_plus_coefficients, "                    ")
+    printPolyFromRationals(x_minus_log_1_plus_variable_name, x_minus_log_1_plus_coefficients, x_minus_log_1_plus_indent)
 
     print("(12) - exp(x) - 1:")
     exp_minus_1_coefficients = [exp_minus_1_coefficient(k) for k in range(0, exp_minus_1_series_length)]
-    printPolyFromRationals(exp_minus_1_variable_name, exp_minus_1_coefficients, "                    ")
+    printPolyFromRationals(exp_minus_1_variable_name, exp_minus_1_coefficients, exp_minus_1_indent)
 
     print("(13) - ((exp(x) - 1) / x - 1) / x - 0.5:")
     exp_minus_1_ratio_minus_1_ratio_minus_half_coefficients = [exp_minus_1_ratio_minus_1_ratio_minus_half_coefficient(k) for k in range(0, exp_minus_1_ratio_minus_1_ratio_minus_half_series_length)]
-    printPolyFromRationals(exp_minus_1_ratio_minus_1_ratio_minus_half_variable_name, exp_minus_1_ratio_minus_1_ratio_minus_half_coefficients, "                    ")
+    printPolyFromRationals(exp_minus_1_ratio_minus_1_ratio_minus_half_variable_name, exp_minus_1_ratio_minus_1_ratio_minus_half_coefficients, exp_minus_1_ratio_minus_1_ratio_minus_half_indent)
 
     print("(14) - log(exp(x) - 1) / x:")
     log_exp_minus_1_ratio_coefficients = get_log_exp_minus_1_ratio_coefficients(log_exp_minus_1_ratio_series_length)
-    printPolyFromRationals(log_exp_minus_1_ratio_variable_name, log_exp_minus_1_ratio_coefficients, "                    ")
+    printPolyFromRationals(log_exp_minus_1_ratio_variable_name, log_exp_minus_1_ratio_coefficients, log_exp_minus_1_ratio_indent)
 
     print("(15) - normcdfln asymptotic:")
     normcdfln_asymptotic_coefficients = [normcdfln_asymptotic_coefficient(k) for k in range(0, normcdfln_asymptotic_series_length)]
-    printPolyFromRationals(normcdfln_asymptotic_variable_name, normcdfln_asymptotic_coefficients, "                    ")
+    printPolyFromRationals(normcdfln_asymptotic_variable_name, normcdfln_asymptotic_coefficients, normcdfln_asymptotic_indent)
 
 if __name__ == '__main__': main()
