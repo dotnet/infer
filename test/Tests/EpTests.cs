53794a84b84e47ce2610f0b3cd8d6dcc4eafcc86
Tom Minka <8955276+tminka@users.noreply.github.com>
2020-03-03 18:57:36 +0000

    Added TruncatedGammaPowerTest

--- test/Tests/EpTests.cs
+++ test/Tests/EpTests.cs
@@ -39,6 +39,13 @@ namespace Microsoft.ML.Probabilistic.Tests
         }
 
         [Fact]
+        public void TruncatedGammaPowerTest()
+        {
+            Assert.True(PowerOp.PowAverageConditional(new TruncatedGamma(5.196e+48, 5.567e-50, 1, double.PositiveInfinity), 0.0016132617913803061, GammaPower.Uniform(-1)).IsProper());
+            Assert.True(PowerOp.PowAverageConditional(new TruncatedGamma(23.14, 0.06354, 1, double.PositiveInfinity), 1.5543122344752203E-15, GammaPower.Uniform(-1)).IsProper());
+        }
+
+        [Fact]
         public void TruncatedGammaPower_ReturnsGammaShapeGreaterThan1()
         {
             Variable<TruncatedGamma> xPriorVar = Variable.Observed(default(TruncatedGamma)).Named("xPrior");
@@ -202,8 +209,6 @@ namespace Microsoft.ML.Probabilistic.Tests
         [Fact]
         public void GammaPowerPowerTest()
         {
-            Assert.True(PowerOp.PowAverageConditional(new TruncatedGamma(5.196e+48, 5.567e-50, 1, double.PositiveInfinity), 0.0016132617913803061, GammaPower.Uniform(-1)).IsProper());
-            Assert.True(PowerOp.PowAverageConditional(new TruncatedGamma(23.14, 0.06354, 1, double.PositiveInfinity), 1.5543122344752203E-15, GammaPower.Uniform(-1)).IsProper());
             Assert.False(double.IsNaN(PowerOp.GammaPowerFromDifferentPower(new GammaPower(1.333, 1.5, 1), 0.01).Shape));
             Assert.True(PowerOp.XAverageConditional(new GammaPower(7, 0.1111, -1), new GammaPower(16.19, 0.06154, 1), 2.2204460492503136E-16, GammaPower.Uniform(1)).IsProper());
             Assert.True(PowerOp.PowAverageConditional(GammaPower.FromShapeAndRate(9.0744065303642287, 8.7298765698182414, 1), 1.6327904641199278, GammaPower.Uniform(-1)).IsProper());
