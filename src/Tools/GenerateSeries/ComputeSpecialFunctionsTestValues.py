c248951afd12baf000e8f0e76d18e14de4711a07
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2020-03-16 18:35:10 +0300

    Work on test values generation

--- src/Tools/GenerateSeries/ComputeSpecialFunctionsTestValues.py
+++ src/Tools/GenerateSeries/ComputeSpecialFunctionsTestValues.py
@@ -3,45 +3,51 @@
 # See the LICENSE file in the project root for more information.
 from __future__ import division
 from sympy import *
+from sympy.stats import *
 import os
 import csv
+import mpmath
 
-def tetragamma(x):
-    return polygamma(2, x)
+def normal_cdf_moment_ratio(n, x):
+    #TODO: fix
+    mpmath.mp.dps = 500
+    xmpf = x.num
+    nmpf = n.num
+    return Float(mpmath.exp(xmpf * xmpf / 4) * mpmath.pcfu(0.5 + nmpf, -xmpf))
 
 pair_info = {
     'BesselI.csv': besseli,
     'BetaCdf.csv': None,
     'Digamma.csv': digamma,
     'Erfc.csv': erfc,
-    'ExpMinus1.csv': None,
-    'ExpMinus1RatioMinus1RatioMinusHalf.csv': None,
+    'ExpMinus1.csv': lambda x: exp(x) - 1,
+    'ExpMinus1RatioMinus1RatioMinusHalf.csv': lambda x: ((exp(x) - 1) / x - 1) / x - S(1) / 2,
     'Gamma.csv': gamma,
     'GammaLn.csv': loggamma,
-    'GammaLower.csv': None, #lowergamma,
-    'GammaUpper.csv': None,
-    'GammaUpperRegularized.csv': None,
-    'GammaUpperScale.csv' : None,
-    'Log1MinusExp.csv': None,
-    'Log1Plus.csv': None,
-    'LogExpMinus1.csv': None,
-    'Logistic.csv': None,
+    'GammaLower.csv': lambda s, x: lowergamma(s, x) / gamma(s) if s != oo else S(0),
+    'GammaUpper.csv': uppergamma,
+    'GammaUpperRegularized.csv': lambda s, x: 1 - (lowergamma(s, x) / gamma(s) if s != oo else S(0)),
+    'GammaUpperScale.csv' : lambda s, x: x ** s * exp(-x) / gamma(s),
+    'Log1MinusExp.csv': lambda x: log(1 - exp(x)),
+    'Log1Plus.csv': lambda x: log(1 + x),
+    'LogExpMinus1.csv': lambda x: log(exp(x) - 1),
+    'Logistic.csv': lambda x: 1 / (1 + exp(-x)),
     'logisticGaussian.csv': None,
     'logisticGaussianDeriv.csv': None,
     'logisticGaussianDeriv2.csv': None,
-    'LogisticLn.csv': None,
-    'LogSumExp.csv': None,
-    'NormalCdf.csv': None,
-    'NormalCdf2.csv': None,
+    'LogisticLn.csv': lambda x: -log(1 + exp(-x)),
+    'LogSumExp.csv': lambda x, y: log(exp(x) + exp(y)),
+    'NormalCdf.csv': lambda x: erfc(-x / sqrt(S(2))) / 2,
+    'NormalCdf2.csv': None, # lambda x, y, r: integrate(1 / (2 * pi * sqrt(1 - sx * sx)) * exp(-(x * x + y * y - 2 * sx * x * y) / (2 * (1 - sx * sx))), (sx, -1, r)),
     'NormalCdfIntegral.csv': None,
     'NormalCdfIntegralRatio.csv': None,
-    'NormalCdfInv.csv': None,
-    'NormalCdfLn.csv': None,
+    'NormalCdfInv.csv': lambda x: -sqrt(S(2)) * erfcinv(2 * x),
+    'NormalCdfLn.csv': lambda x: log(erfc(-x / sqrt(S(2))) / 2),
     'NormalCdfLn2.csv': None,
-    'NormalCdfLogit.csv': None,
-    'NormalCdfMomentRatio.csv': None,
+    'NormalCdfLogit.csv': lambda x: log(erfc(-x / sqrt(S(2))) / 2) - log(erfc(x / sqrt(S(2))) / 2),
+    'NormalCdfMomentRatio.csv': normal_cdf_moment_ratio,
     'NormalCdfRatioLn2.csv': None,
-    'Tetragamma.csv': tetragamma,
+    'Tetragamma.csv': lambda x: polygamma(2, x),
     'Trigamma.csv': trigamma,
     'ulp.csv': None
     }
