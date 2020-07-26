e00a83e29fa1135381238c381e69a5bcfbbcf6e4
Tom Minka <8955276+tminka@users.noreply.github.com>
2020-07-26 22:07:20 +0100

    Update to xUnit 2.4.1 (#265)
    * Update build instructions

--- test/netcoretest.sh
+++ test/netcoretest.sh
@@ -16,7 +16,6 @@ then
 fi
 
 compath=/bin/${configuration}/netcoreapp3.1/
-projects="Learners/LearnersTests Tests TestPublic TestFSharp"
 
 # filter for parallel test run
 #parallel_filter='-notrait Platform=x86 -notrait Category=OpenBug -notrait Category=BadTest -notrait Category=CompilerOptionsTest -notrait Category=CsoftModel -notrait Category=ModifiesGlobals -notrait Category=DistributedTest -notrait Category=Performance'
@@ -27,28 +26,22 @@ parallel_filter='--filter (Platform!=x86)&(Category!=OpenBug)&(Category!=BadTest
 sequential_filter='--filter (Platform!=x86)&(Category!=OpenBug)&(Category!=BadTest)&(Category!=CompilerOptionsTest)&(Category=CsoftModel|Category=ModifiesGlobals|Category=DistributedTests|Category=Performance)'
 
 exitcode=0
-index=0
 
 echo -e "\033[44;37m=====================PARALLEL TESTS RUNNING============================\033[0m"
 cp "Tests/parallel.xunit.runner.json" "Tests/xunit.runner.json"
-for project in $projects
-do
-    # Please note that due to xUnit issue we need to run tests for each project separately
-    dotnet test "$project" -c "$configuration" $parallel_filter --logger "trx;logfilename=netcoretest-result${index}.trx"
-    if [ 0 -ne $? ]
-    then
-        echo -e "\033[5;41;1;37mParallel running failure!\033[0m"
-        exitcode=1
-    else
-        echo -e "\033[32;1mParallel running success!\033[0m"
-    fi
-    (( index++ ))
-done
+dotnet test ../Infer.sln -c "$configuration" $parallel_filter --logger "trx;logfilename=netcoretest-result.trx"
+if [ 0 -ne $? ]
+then
+    echo -e "\033[5;41;1;37mParallel running failure!\033[0m"
+    exitcode=1
+else
+    echo -e "\033[32;1mParallel running success!\033[0m"
+fi
 
 echo -e "\033[44;37m=====================SEQUENTIAL TESTS RUNNING=========================\033[0m"
 # See https://xunit.github.io/docs/configuration-files
 cp "Tests/sequential.xunit.runner.json" "Tests/xunit.runner.json"
-dotnet test Tests -c "$configuration" $sequential_filter --logger "trx;logfilename=netcoretest-result${index}.trx"
+dotnet test Tests -c "$configuration" $sequential_filter --logger "trx;logfilename=netcoretest-result2.trx"
 if [ 0 -ne $? ]
 then
     echo -e "\033[5;41;1;37mSequential running failure!\033[0m"
