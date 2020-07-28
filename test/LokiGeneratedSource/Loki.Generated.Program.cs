d06eb8be8914ad78448edb8a3fec48e1c72883e5
Dmitry Kats <ratkillerx@hotmail.com>
2020-07-28 12:26:46 +0300

    Set up Loki.Generated to trun all tests

--- test/LokiGeneratedSource/Loki.Generated.Program.cs
+++ test/LokiGeneratedSource/Loki.Generated.Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Linq;
 using Microsoft.ML.Probabilistic.Tests;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Loki.Generated
 {
@@ -14,14 +15,14 @@ namespace Loki.Generated
             var logFilePath = Path.Combine(Environment.CurrentDirectory, "log.csv");
             Environment.CurrentDirectory = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "Tests"));
 
-            var result = await TestRunner.RunTestAsync(new ConstrainEqualTests().ConstrainEqualCaseArrayTest, 0);
-            Console.WriteLine($"{result.ContainingTypeFullName}.{result.TestName}, {result.StartingFuel} fuel units: {result.Outcome}\n{result.Message}");
-
-            //var tests = TestInfo.FromAssembly(typeof(Program).Assembly)
-            //    .Where(ti => !ti.Traits.Any(trait => trait.Key == "Category" && trait.Value == "CompilerOptionsTest"))
-            //    //.Skip(100)
-            //    .Take(100)
-            //    ;
+            //var result = await TestRunner.RunTestAsync(new ConstrainEqualTests().ConstrainEqualCaseArrayTest, 0);
+            //Console.WriteLine($"{result.ContainingTypeFullName}.{result.TestName}, {result.StartingFuel} fuel units: {result.Outcome}\n{result.Message}");
+            var categoriesToSkip = new[] { "CompilerOptionsTest", "Performance", "OpenBug", "BadTest" };
+            var tests = TestInfo.FromAssembly(typeof(Program).Assembly)
+                .Where(ti => !ti.Traits.Any(trait => trait.Key == "Category" && categoriesToSkip.Contains(trait.Value)))
+                //.Skip(100)
+                //.Take(100)
+                ;
 
             ////var tests = new TestInfo[] { TestInfo.FromDelegate(new BayesPointMachineTests().BayesPointEvidence2) }
             ////    .Where(ti => !ti.Traits.Any(trait => trait.Key == "Category" && trait.Value == "CompilerOptionsTest"))
@@ -29,28 +30,29 @@ namespace Loki.Generated
             ////    //.Take(100)
             ////    ;
 
-            //var testRun = TestSuiteRunner.RunTestSuite(
-            //    tests,
-            //    new[] { 0ul },
-            //    logFilePath,
-            //    true,
-            //    TimeSpan.FromMinutes(0.5),
-            //    4,
-            //    new Progress<TestSuiteRunStatus>(s => Console.Title = $"Completed {s.CompletedTests} tests. OpCounts: {string.Join(" | ", s.CurrentTestStatuses.Select(cts => cts.CurrentRunStatus.OperationCount.ToString()))}"));
+            var testRun = TestSuiteRunner.RunTestSuite(
+                tests,
+                new[] { ulong.MaxValue, 0ul },
+                logFilePath,
+                false,
+                TimeSpan.FromMinutes(10),
+                16,
+                true,
+                new Progress<TestSuiteRunStatus>(s => Console.Title = $"Completed {s.CompletedTests} tests. OpCounts: {string.Join(" | ", s.CurrentTestStatuses.Select(cts => cts.CurrentRunStatus.OperationCount.ToString()))}"));
 
-            //var testResultEnumerator = testRun.GetAsyncEnumerator();
-            //try
-            //{
-            //    while (await testResultEnumerator.MoveNextAsync())
-            //    {
-            //        var result = testResultEnumerator.Current;
-            //        Console.WriteLine($"{result.ContainingTypeFullName}.{result.TestName}, {result.StartingFuel} fuel units: {result.Outcome}\n{result.Message}");
-            //    }
-            //}
-            //finally
-            //{
-            //    await testResultEnumerator.DisposeAsync();
-            //}
+            var testResultEnumerator = testRun.GetAsyncEnumerator();
+            try
+            {
+                while (await testResultEnumerator.MoveNextAsync())
+                {
+                    var result = testResultEnumerator.Current;
+                    Console.WriteLine($"{result.ContainingTypeFullName}.{result.TestName}, {result.StartingFuel} fuel units: {result.Outcome}\n{result.Message}");
+                }
+            }
+            finally
+            {
+                await testResultEnumerator.DisposeAsync();
+            }
         }
     }
 }
