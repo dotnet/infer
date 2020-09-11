80b02d3d93499ff8be2af0a675488e2c7a5abc52
Tom Minka <8955276+tminka@users.noreply.github.com>
2020-09-11 18:50:50 +0100

    SimplestBackwardChainTest3

--- test/TestApp/Program.cs
+++ test/TestApp/Program.cs
@@ -82,6 +82,8 @@ namespace TestApp
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
+            new SerialTests().SimplestBackwardChainTest3();
+
             if (false)
             {
                 // Run all tests (need to run in 64-bit else OutOfMemory due to loading many DLLs)
--- test/Tests/SerialTests.cs
+++ test/Tests/SerialTests.cs
@@ -2350,6 +2350,53 @@ namespace Microsoft.ML.Probabilistic.Tests
         }
 
         [Fact]
+        public void SimplestBackwardChainTest3()
+        {
+            int length = 10;
+            var lengthVar = Variable.Observed(length).Named("length");
+
+            Range rows = new Range(lengthVar).Named("i");
+            VariableArray<double> states = Variable.Array<double>(rows).Named("states");
+
+            using (ForEachBlock rowBlock = Variable.ForEach(rows))
+            {
+                var index = rowBlock.Index;
+                var isLast = (index == lengthVar - 1);
+                var lengthVarMinus1 = (lengthVar - 1).Named("lengthVarMinus1");
+                using (Variable.If(index == lengthVarMinus1))
+                {
+                    states[rowBlock.Index] = Variable.GaussianFromMeanAndVariance(1, 1);
+                }
+                using (Variable.If(index < lengthVarMinus1))
+                {
+                    states[rowBlock.Index] = Variable.GaussianFromMeanAndVariance(states[rowBlock.Index + 1], 1);
+                }
+            }
+
+            InferenceEngine engine = new InferenceEngine();
+            engine.NumberOfIterations = 1;
+            Console.WriteLine("After {0} Iterations", engine.NumberOfIterations);
+            var result1 = engine.Infer<Gaussian[]>(states);
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("state[{0}] = {1}", i, result1[i]);
+            }
+            for (int i = 0; i < length; i++)
+            {
+                Assert.Equal(result1[i].GetVariance(), length - i, 1e-8);
+            }
+            engine.NumberOfIterations = 10;
+            int count = 0;
+            engine.ProgressChanged += delegate (InferenceEngine sender, InferenceProgressEventArgs progress)
+            {
+                count++;
+            };
+            engine.Infer(states);
+            Console.WriteLine("iter count = {0} should be 0", count);
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
         public void SimplestChainWithObservationsTest()
         {
             int length = 10;
