9aa76a0fbbaab0af2b497238267cf1ee16a28cb7
Jonathan Tims <jotims@microsoft.com>
2020-09-30 14:39:59 +0100

    wip

---
+++ test/TestAllCompilerOptions/Program.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.ML.Probabilistic.Tests.TestAllCompilerOptions
+{
+    using Xunit;
+
+    class Program
+    {
+        static void Main()
+        {
+            var failed = TestUtils.TestAllCompilerOptions();
+            Assert.Equal(0, failed);
+        }
+    }
+}
