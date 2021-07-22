664765e45ef7c0167d6449f6fee731e1b377b0d1
Dmitry Kats <v-dmkats@microsoft.com>
2021-07-22 16:12:14 +0300

    Fixed the difference between SequenceDistribution.EnumerateSupport and TryEnumerateSupport; removed some code duplication

--- src/Runtime/Distributions/SequenceDistribution.cs
+++ src/Runtime/Distributions/SequenceDistribution.cs
@@ -188,13 +188,7 @@ namespace Microsoft.ML.Probabilistic.Distributions
         /// <inheritdoc cref="FromWeightFunction(TWeightFunction)"/>
         [Construction(nameof(ToAutomaton), UseWhen = nameof(UsesAutomatonRepresentation))]
         public static TThis FromWeightFunction(TAutomaton weightFunction)
-        {
-            Argument.CheckIfNotNull(weightFunction, nameof(weightFunction));
-
-            var result = new TThis();
-            result.SetWeightFunction(weightFunction);
-            return result;
-        }
+            => FromWeightFunction(WeightFunctionFactory.FromAutomaton(weightFunction));
 
         /// <summary>
         /// Creates a distribution which puts all probability mass on the empty sequence.
@@ -859,17 +853,7 @@ namespace Microsoft.ML.Probabilistic.Distributions
 
         /// <inheritdoc cref="SetWeightFunction(TWeightFunction,bool)"/>
         public void SetWeightFunction(TAutomaton weightFunction, bool normalizeStructure = true)
-        {
-            Argument.CheckIfNotNull(weightFunction, nameof(weightFunction));
-
-            this.sequenceToWeight = WeightFunctionFactory.FromAutomaton(weightFunction);
-            this.isNormalized = false;
-
-            if (normalizeStructure)
-            {
-                this.NormalizeStructure();
-            }
-        }
+            => SetWeightFunction(WeightFunctionFactory.FromAutomaton(weightFunction), normalizeStructure);
 
         /// <summary>
         /// Replaces the weight function of the current distribution with a given one.
@@ -948,23 +932,14 @@ namespace Microsoft.ML.Probabilistic.Distributions
         /// Only point mass elements are supported.
         /// </summary>
         /// <param name="maxCount">The maximum support enumeration count.</param>
-        /// <param name="tryDeterminize">Try to determinize if this is a string automaton</param>
+        /// <param name="tryDeterminize">Try to determinize if this distribution uses
+        /// a string automaton representation.</param>
         /// <exception cref="AutomatonException">Thrown if enumeration is too large.</exception>
         /// <returns>The strings supporting this distribution</returns>
         public IEnumerable<TSequence> EnumerateSupport(int maxCount = 1000000, bool tryDeterminize = true)
         {
-            if (tryDeterminize
-                && sequenceToWeight.UsesAutomatonRepresentation
-                && sequenceToWeight.AsAutomaton() is StringAutomaton sa
-                && sa.Data.IsEnumerable != false)
-            {
-                // Determinization of automaton may fail if distribution is not normalized.
-                this.EnsureNormalized();
-                sequenceToWeight.AsAutomaton().TryDeterminize(out var determinizedAutomaton);
-                // Sometimes automaton is not determinized, but is still made epsilon-free,
-                // which is a nice optimization to keep.
-                sequenceToWeight = WeightFunctionFactory.FromAutomaton(determinizedAutomaton);
-            }
+            if (tryDeterminize)
+                TryDeterminizeBeforeSupportEnumeration();
 
             return this.sequenceToWeight.EnumerateSupport(maxCount);
         }
@@ -975,21 +950,14 @@ namespace Microsoft.ML.Probabilistic.Distributions
         /// </summary>
         /// <param name="maxCount">The maximum support enumeration count.</param>
         /// <param name="result">The strings supporting this distribution.</param>
-        /// <param name="tryDeterminize">Try to determinize if this is a string automaton</param>
+        /// <param name="tryDeterminize">Try to determinize if this distribution uses
+        /// a string automaton representation.</param>
         /// <exception cref="AutomatonException">Thrown if enumeration is too large.</exception>
         /// <returns>True if successful, false otherwise.</returns>
         public bool TryEnumerateSupport(int maxCount, out IEnumerable<TSequence> result, bool tryDeterminize = true)
         {
-            if (tryDeterminize
-                && sequenceToWeight.UsesAutomatonRepresentation 
-                && sequenceToWeight.AsAutomaton() is StringAutomaton sa
-                && sa.Data.IsEnumerable != false)
-            {
-                // Determinization of automaton may fail if distribution is not normalized.
-                this.EnsureNormalized();
-                if (sequenceToWeight.AsAutomaton().TryDeterminize(out var determinizedAutomaton))
-                    sequenceToWeight = WeightFunctionFactory.FromAutomaton(determinizedAutomaton);
-            }
+            if (tryDeterminize)
+                TryDeterminizeBeforeSupportEnumeration();
 
             return this.sequenceToWeight.TryEnumerateSupport(maxCount, out result);
         }
@@ -1725,6 +1693,21 @@ namespace Microsoft.ML.Probabilistic.Distributions
         private void NormalizeStructure()
         {
             sequenceToWeight = sequenceToWeight.NormalizeStructure();
+        }
+
+        private void TryDeterminizeBeforeSupportEnumeration()
+        {
+            if (sequenceToWeight.UsesAutomatonRepresentation
+                    && sequenceToWeight.AsAutomaton() is StringAutomaton sa
+                    && sa.Data.IsEnumerable != false)
+            {
+                // Determinization of automaton may fail if distribution is not normalized.
+                this.EnsureNormalized();
+                sequenceToWeight.AsAutomaton().TryDeterminize(out var determinizedAutomaton);
+                // Sometimes automaton is not determinized, but is still made epsilon-free,
+                // which is a nice optimization to keep.
+                sequenceToWeight = WeightFunctionFactory.FromAutomaton(determinizedAutomaton);
+            }
         }
 
         #endregion
