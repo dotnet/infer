ac3c5db7fc61165616526b0b20a0da70671b05b4
Ivan Korostelev <ivkorost@microsoft.com>
2021-09-29 12:45:18 +0100

    Fix tests

--- test/Tests/Strings/AutomatonTests.cs
+++ test/Tests/Strings/AutomatonTests.cs
@@ -962,7 +962,9 @@ namespace Microsoft.ML.Probabilistic.Tests
                 // Fine, because 200k < default limit
                 unlimited.CheckStateCount(automaton);
 
-                for (var i = 1; i < 200000; ++i)
+                builder = new StringAutomaton.Builder();
+                state = builder.Start;
+                for (var i = 1; i < 400000; ++i)
                 {
                     state = state.AddTransition('a', Weight.One);
                 }
