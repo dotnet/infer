c425b689b92e9d56172855430103505a36a8b7fe
Tom Minka <8955276+tminka@users.noreply.github.com>
2021-11-29 17:28:00 +0000

    Undo

--- test/TestApp/Program.cs
+++ test/TestApp/Program.cs
@@ -83,8 +83,6 @@ namespace TestApp
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            BlogTests.TrueSkill2Test();
-
             if (false)
             {
                 // Run all tests (need to run in 64-bit else OutOfMemory due to loading many DLLs)
