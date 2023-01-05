4935d2267732e8915e157281ddbd5b03ab9f44d0
Jonathan Tims <jotims@microsoft.com>
2023-01-05 13:38:10 +0000

    w

--- test/TestApp/Program.cs
+++ test/TestApp/Program.cs
@@ -48,9 +48,7 @@ namespace TestApp
             Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
             var logWriter = new StreamWriter("debug.txt");
             Trace.Listeners.Add(new TextWriterTraceListener(logWriter));
-#if NETFRAMEWORK
-            InferenceEngine.Visualizer = new WindowsVisualizer();
-#elif WINDOWS
+#if NETFRAMEWORK || WINDOWS
             InferenceEngine.Visualizer = new WindowsVisualizer();
 #endif
             Debug.AutoFlush = true;
