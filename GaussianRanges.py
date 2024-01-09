6148fe66af6eb1752e612ef992d132a6dfb2ec26
Victor M <numero1@gmail.com>
2024-01-09 18:03:17 +0100

    Adapted for ironpython3
    I've modified it to work with ironython3 if you encounter any issues please comment

---
+++ GaussianRanges.py
@@ -0,0 +1,37 @@
+# Licensed to the .NET Foundation under one or more agreements.
+# The .NET Foundation licenses this file to you under the MIT license.
+# See the LICENSE file in the project root for more information.
+#-----------------------------------------------------------------------------------
+# Infer.NET IronPython example: Learning a Gaussian
+#-----------------------------------------------------------------------------------
+import System
+import InferNetWrapper
+from InferNetWrapper import *
+
+#print(dir(System))
+from System import Int32 as intG #soluciona el error int linea 18
+
+def gaussian_ranges():
+    print("\n\n------------------ Infer.NET Learning a Gaussian example ------------------\n");
+
+    # The model
+    len = Variable.New[intG]()
+    dataRange = Range(len)
+    x = Variable.Array[float](dataRange)
+    mean = Variable.GaussianFromMeanAndVariance(0, 100)
+    precision = Variable.GammaFromShapeAndScale(1, 1)
+    x[dataRange] = Variable.GaussianFromMeanAndPrecision(mean, precision).ForEach(dataRange)
+
+    # The data
+    data = System.Array.CreateInstance(float, 100)
+    for i in range(0,100):
+        data[i] = Rand.Normal(0, 1)
+
+    # Binding the data
+    len.ObservedValue = 100
+    x.ObservedValue = data
+
+    # The inference
+    ie = InferenceEngine(Algorithms.VariationalMessagePassing())
+    print ("mean = ", ie.Infer(mean))
+    print ("prec = ", ie.Infer(precision))
