6ccf3b3ff8e00661abe99dd4735cceaacf8470a5
Andrii Kurdiumov <kant2002@gmail.com>
2024-03-11 11:08:09 +0600

    Add support for running on .NET 9 with Roslyn
    Also refactor, to no longer account for any new .NET versions

--- src/Compiler/TransformFramework/CodeTransformer.cs
+++ src/Compiler/TransformFramework/CodeTransformer.cs
@@ -98,11 +98,8 @@ namespace Microsoft.ML.Probabilistic.Compiler
         /// The language that we'll compile
         /// </summary>
         protected string language = "C#";
-        protected bool runningOnNetCore = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Core", StringComparison.OrdinalIgnoreCase)
-            || System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 5", StringComparison.OrdinalIgnoreCase)
-            || System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 6", StringComparison.OrdinalIgnoreCase)
-            || System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 7", StringComparison.OrdinalIgnoreCase)
-            || System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET 8", StringComparison.OrdinalIgnoreCase);
+        protected bool runningOnNetCore = !System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework", StringComparison.OrdinalIgnoreCase)
+            && Type.GetType("Mono.Runtime") == null;
         protected bool runningOnMono = Type.GetType("Mono.Runtime") != null;
 
         /// <summary>
